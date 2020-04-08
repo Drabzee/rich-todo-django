c7b6e5909d389ca74ed36328a129fd0f321dfd8c
Drabzee <uddeshya_kumar@ymail.com>
2020-04-08 17:05:00 +0530

    Redirect method changed on frontend

--- myapp/static/addTodo.js
+++ myapp/static/addTodo.js
@@ -14,7 +14,7 @@ function submitForm() {
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                window.location.href = "http://127.0.0.1:8000"
+                location.reload();
             }
         };
         xhttp.open("POST", "/add-todo", true);
